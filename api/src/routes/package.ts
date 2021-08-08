a85d94c7cd648699ce23d49797f06b91a26142c7
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-08-08 14:23:12 -0700

    refactor(api): started to refactor API to use a real database (WIP)

--- api/src/routes/package.ts
+++ api/src/routes/package.ts
@@ -1,99 +1,168 @@
+import { Package, prisma } from "@prisma/client";
 import { Request, Response } from "express";
 import { METHOD, routeConfig } from "../..";
+import { db } from "../db/Database";
 
-interface PackageInstall {
-  macos?: {
-    url: string;
-    type: "dmg" | "app";
-    checksum?: string;
-  }
-  windows?: {
-    url: string;
-    type: "exe" | "msi";
-    checksum?: string;
-  }
-  linux?: {
-    url: string;
-    type: "deb";
-    checksum?: string;
-  }
-}
+// interface PackageInstall {
+//   macos?: {
+//     url: string;
+//     type: "dmg" | "app";
+//     checksum?: string;
+//   }
+//   windows?: {
+//     url: string;
+//     type: "exe" | "msi";
+//     checksum?: string;
+//   }
+//   linux?: {
+//     url: string;
+//     type: "deb";
+//     checksum?: string;
+//   }
+// }
 
-interface Package {
-  name: string;
-  description: string;
+// interface Package {
+//   name: string;
+//   description: string;
 
-  install: PackageInstall;
-}
+//   install: PackageInstall;
+// }
 
-const packages = new Map<string, Package>();
-packages.set('chrome', {
-  name: "chrome",
-  description: "The Google Chrome browser",
-  install: {
-    macos: {
-      url: "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
-      type: "dmg",
+//Temporary package creation queries
+(async () => {
+  const existingPackage = await db.package.findFirst({
+    where: {
+      name: "chrome",
     },
-    windows: {
-      url: "https://dl.google.com/chrome/install/1/GGRO/googlechrome.exe",
-      type: "exe",
-    },
-    linux: {
-      url: "https://dl.google.com/chrome/install/1/GGRO/google-chrome",
-      type: "deb",
-    }
+  });
+
+  if (existingPackage) {
+    return;
+  } else {
+    return await db.package.create({
+      data: {
+        name: "chrome",
+        description: "The Google Chrome browser",
+        version: "1.0.0",
+        install: {
+          create: {
+            macos: {
+              create: {
+                url: "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
+                type: "dmg",
+              },
+            },
+
+            windows: {
+              create: {
+                url: "https://dl.google.com/chrome/install/1/GGRO/googlechrome.exe",
+                type: "exe",
+              },
+            },
+
+            linux: {
+              create: {
+                url: "https://dl.google.com/chrome/install/1/GGRO/google-chrome",
+                type: "deb",
+              },
+            },
+          },
+        },
+      },
+    });
   }
-});
-packages.set('skyline', {
-  name: "skyline",
-  description: "The Super Fast Code Editor",
-  install: {
-    macos: {
-      url: "https://github.com/skyline-editor/skyline/releases/download/v0.1.0/Skyline_0.1.0_x64.dmg",
-      type: "dmg",
-    },
-    windows: {
-      url: "https://github.com/skyline-editor/skyline/releases/download/v0.1.0/Skyline_0.1.0_x64.msi",
-      type: "msi",
-    },
-  }
-});
+})();
+
+// const packages = new Map<string, Package>();
+// packages.set("chrome", {
+//   name: "chrome",
+//   description: "The Google Chrome browser",
+//   install: {
+//     macos: {
+//       url: "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
+//       type: "dmg",
+//     },
+//     windows: {
+//       url: "https://dl.google.com/chrome/install/1/GGRO/googlechrome.exe",
+//       type: "exe",
+//     },
+//     linux: {
+//       url: "https://dl.google.com/chrome/install/1/GGRO/google-chrome",
+//       type: "deb",
+//     },
+//   },
+// });
+// packages.set("skyline", {
+//   name: "skyline",
+//   description: "The Super Fast Code Editor",
+//   install: {
+//     macos: {
+//       url: "https://github.com/skyline-editor/skyline/releases/download/v0.1.0/Skyline_0.1.0_x64.dmg",
+//       type: "dmg",
+//     },
+//     windows: {
+//       url: "https://github.com/skyline-editor/skyline/releases/download/v0.1.0/Skyline_0.1.0_x64.msi",
+//       type: "msi",
+//     },
+//   },
+// });
 
 class PackageController {
   @routeConfig({
     method: METHOD.GET,
-    path: "/package/"
+    path: "/package/",
   })
-  public get(
-    request: Request,
-    response: Response
-  ): Package {
-    const package_id = request.query.id?.toString()?.toLowerCase();
-    if (!package_id) throw new Error(`"id" is required`);
+  public async get(request: Request, response: Response): Promise<Package> {
+    const packageID = request.query.id?.toString()?.toLowerCase();
+    if (!packageID) throw new Error(`"id" is required`);
 
-    if (!packages.has(package_id)) throw new Error(`Couldn't find package "${package_id}"`);
-    return packages.get(package_id)!;
+    const packageData = await db.package.findUnique({
+      where: {
+        id: packageID,
+      },
+    });
+
+    if (!packageData) {
+      throw new Error(`Couldn't find package "${packageID}"`);
+    } else {
+      return packageData as Package;
+    }
   }
 
   @routeConfig({
     method: METHOD.GET,
-    path: "/package/install"
+    path: "/package/install",
   })
-  public install(
-    request: Request,
-    response: Response
-  ): string {
-    const package_id = request.query.package?.toString().toLowerCase();
+  public async install(request: Request, response: Response): Promise<strin> {
+    const packageID = request.query.package?.toString().toLowerCase();
     const platform = request.query.platform?.toString().toLowerCase();
 
-    if (!package_id) throw new Error(`"package" is required`);
-    if (!platform) throw new Error(`"platform" is required`)
-    
-    if (!packages.has(package_id)) throw new Error(`Couldn't find package "${package_id}"`);
-    const package_data = packages.get(package_id)!;
+    if (!packageID) throw new Error(`"package" is required`);
+    if (!platform) throw new Error(`"platform" is required`);
 
-    if (!(platform in package_data.install)) throw new Error(`Package "${package_id}", cant be installed on your system`);
+    const packageData = await db.package.findUnique({
+      where: {
+        id: packageID,
+      },
+      include: {
+        install: {
+          include: {
+            macos: true,
+            windows: true,
+            linux: true,
+          },
+        },
+      },
+    });
+
+    if (!packageData) throw new Error(`Couldn't find package "${packageID}"`);
+    if (!packageData.install[platform]) throw new Error("");
+
+    if (!(platform in package_data.install))
+      throw new Error(
+        `Package "${package_id}", cant be installed on your system`
+      );
+
     return package_data.install[platform as keyof PackageInstall]!.url;
   }
 }
