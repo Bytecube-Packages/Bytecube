8b5a1c0af8dec89797c9506b7ea65c230fec551f
firecraftgaming <eliyah@sundstroem.com>
2021-08-08 13:20:05 +0200

    feat(api): added multiple package support

--- api/src/routes/package.ts
+++ api/src/routes/package.ts
@@ -1,28 +1,64 @@
 import { Request, Response } from "express";
 import { METHOD, routeConfig } from "../..";
 
+interface PackageInstall {
+  macos?: {
+    url: string;
+    type: "dmg" | "app";
+    checksum?: string;
+  }
+  windows?: {
+    url: string;
+    type: "exe" | "msi";
+    checksum?: string;
+  }
+  linux?: {
+    url: string;
+    type: "deb";
+    checksum?: string;
+  }
+}
+
 interface Package {
   name: string;
   description: string;
 
+  install: PackageInstall;
+}
+
+const packages = new Map<string, Package>();
+packages.set('chrome', {
+  name: "chrome",
+  description: "The Google Chrome browser",
   install: {
-    macos?: {
-      url: string;
-      type: "dmg" | "app";
-      checksum?: string;
-    }
-    windows?: {
-      url: string;
-      type: "exe" | "msi";
-      checksum?: string;
-    }
-    linux?: {
-      url: string;
-      type: "deb";
-      checksum?: string;
+    macos: {
+      url: "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
+      type: "dmg",
+    },
+    windows: {
+      url: "https://dl.google.com/chrome/install/1/GGRO/googlechrome.exe",
+      type: "exe",
+    },
+    linux: {
+      url: "https://dl.google.com/chrome/install/1/GGRO/google-chrome",
+      type: "deb",
     }
   }
-}
+});
+packages.set('skyline', {
+  name: "skyline",
+  description: "The Super Fast Code Editor",
+  install: {
+    macos: {
+      url: "https://github.com/skyline-editor/skyline/releases/download/v0.1.0/Skyline_0.1.0_x64.dmg",
+      type: "dmg",
+    },
+    windows: {
+      url: "https://github.com/skyline-editor/skyline/releases/download/v0.1.0/Skyline_0.1.0_x64.msi",
+      type: "msi",
+    },
+  }
+});
 
 class PackageController {
   @routeConfig({
@@ -35,26 +71,9 @@ class PackageController {
   ): Package {
     const package_id = request.query.id?.toString()?.toLowerCase();
     if (!package_id) throw new Error(`"id" is required`);
-    if (package_id !== 'chrome') throw new Error(`Couldn't find package "${package_id}"`);
 
-    return {
-      name: "chrome",
-      description: "The Google Chrome browser",
-      install: {
-        macos: {
-          url: "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
-          type: "dmg",
-        },
-        windows: {
-          url: "https://dl.google.com/chrome/install/1/GGRO/googlechrome.exe",
-          type: "exe",
-        },
-        linux: {
-          url: "https://dl.google.com/chrome/install/1/GGRO/google-chrome",
-          type: "deb",
-        }
-      }
-    };
+    if (!packages.has(package_id)) throw new Error(`Couldn't find package "${package_id}"`);
+    return packages.get(package_id)!;
   }
 
   @routeConfig({
@@ -71,29 +90,10 @@ class PackageController {
     if (!package_id) throw new Error(`"package" is required`);
     if (!platform) throw new Error(`"platform" is required`)
     
-    if (package_id !== 'chrome') throw new Error(`Couldn't find package "${package_id}"`);
-    if (platform !== 'macos') throw new Error(`Couldn't find package "${package_id}"`);
+    if (!packages.has(package_id)) throw new Error(`Couldn't find package "${package_id}"`);
+    const package_data = packages.get(package_id)!;
 
-    const package_data = {
-      name: "chrome",
-      description: "The Google Chrome browser",
-      install: {
-        macos: {
-          url: "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg",
-          type: "dmg",
-        },
-        windows: {
-          url: "https://dl.google.com/chrome/install/1/GGRO/googlechrome.exe",
-          type: "exe",
-        },
-        linux: {
-          url: "https://dl.google.com/chrome/install/1/GGRO/google-chrome",
-          type: "deb",
-        }
-      }
-    };
-
-    if (!package_data.install[platform]) throw new Error(`Package "${package_id}", cant be installed on your system`);
-    return package_data.install.macos.url;
+    if (!(platform in package_data.install)) throw new Error(`Package "${package_id}", cant be installed on your system`);
+    return package_data.install[platform as keyof PackageInstall]!.url;
   }
 }
