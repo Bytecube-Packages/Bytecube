567a6a276ad577c48abd86dce11b97cf57b00b80
firecraftgaming <eliyah@sundstroem.com>
2021-08-09 21:14:39 +0200

    feat(web): started with implementation of layout

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,11 +1,18 @@
 import { NextPage } from "next";
 
+import styles from "../styles/LandingPage.module.css";
+
 const IndexPage: NextPage = () => (
   <div>
-    <div>
-      <div>Seamlessy</div>
-      <div>develop</div>
-      <div>software</div>
+    <div className={`${styles.navbar}`}>
+      <div className={styles.navbar__logo}/>
+    </div>
+    <div className={`${styles.body}`}>
+      <div className={`${styles.tag}`} >
+        <div className={`${styles.tag__text}`}>Seamlessy</div>
+        <div className={`${styles.tag__text} ${styles.tag_important}`}>develop</div>
+        <div className={`${styles.tag__text}`}>software</div>
+      </div>
     </div>
   </div>
 );
