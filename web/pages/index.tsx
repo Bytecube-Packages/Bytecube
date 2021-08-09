18b4072d826387e6f3856acad0b9e87d141faba7
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-08-09 15:52:18 -0700

    feat(api): publish route but still a WIP

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -5,12 +5,17 @@ import styles from "../styles/LandingPage.module.css";
 const IndexPage: NextPage = () => (
   <div>
     <div className={`${styles.navbar}`}>
-      <div className={styles.navbar__logo}/>
+      <h1>Packages</h1>
+      <h1>Docs</h1>
+      <h1>Blog</h1>
+      <h1>Login</h1>
     </div>
     <div className={`${styles.body}`}>
-      <div className={`${styles.tag}`} >
+      <div className={`${styles.tag}`}>
         <div className={`${styles.tag__text}`}>Seamlessy</div>
-        <div className={`${styles.tag__text} ${styles.tag_important}`}>develop</div>
+        <div className={`${styles.tag__text} ${styles.tag_important}`}>
+          develop
+        </div>
         <div className={`${styles.tag__text}`}>software</div>
       </div>
     </div>
