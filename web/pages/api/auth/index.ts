546fbafadcc10de163e27d3c8cedafce09b00edf
firecraftgaming <eliyah@sundstroem.com>
2021-08-23 22:45:29 +0200

    feat(web): integrated login

---
+++ web/pages/api/auth/index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import QueryString from "qs";
+const domain = 'https://api.bytecube.tk';
+
+export default async function handler(req, res) {
+  if (!req.query.code) return res.status(400).send('No code provided');
+
+  const params = QueryString.stringify({
+    code: req.query.code,
+  });
+
+  console.log(params);
+
+  try {
+    const data = await axios.post(`${domain}/auth/authorize?${params}`);
+    const accessToken = data.data.access_token;
+    
+    res.setHeader('Set-Cookie', `access_token=${accessToken}; Path=/; Secure`);
+    res.redirect('/');
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('An Error Occurred');
+  }
+}
---
+++ web/pages/api/auth/login.ts
@@ -0,0 +1,5 @@
+const domain = 'https://api.bytecube.tk';
+
+export default function handler(req, res) {
+  res.redirect(`${domain}/auth/login`);
+}
