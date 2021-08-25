4ccb0b98c2ac39d850f2dc554862533c34e7c633
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-08-25 07:56:20 -0700

    refactor(web): made listing packages work, but still needs to be styled

--- web/pages/packages/index.tsx
+++ web/pages/packages/index.tsx
@@ -1,7 +1,28 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
 const PackagesPage = () => {
+  const { data: allPackagesData } = useQuery("allPackagesData", async () => {
+    const { data } = await axios.get(`https://api.bytecube.tk/package/list`);
+
+    console.log(data);
+    return data;
+  });
+
   return (
     <div>
-      <h1>Packages</h1>
+      {allPackagesData
+        ? allPackagesData.map((data) => {
+            //have to use "data" instead of "package" because package is a reserved keyword
+            return (
+              <div>
+                <h1>{data.name}</h1>
+                <h1>{data.description}</h1>
+                <h1>{data.version}</h1>
+              </div>
+            );
+          })
+        : ""}
     </div>
   );
 };
